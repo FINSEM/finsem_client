f772e5953200646920230f44f88ae10da5ec4031
dhiraj2803 <dheerajgupta2800@gmail.com>
2022-03-25 23:02:51 +0530

    payment screen

--- lib/ui/screens/transactions_screen/Upcoming/upcoming_payment_screen.dart
+++ lib/ui/screens/transactions_screen/Upcoming/upcoming_payment_screen.dart
@@ -1,14 +1,80 @@
 import 'package:finsem_client/utils/colours.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class UpcomingScreen extends StatelessWidget {
-  const UpcomingScreen({Key? key}) : super(key: key);
+class UpcomingPaymentScreen extends StatelessWidget {
+  const UpcomingPaymentScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: FinColours.secondaryColor,
-      child: Center(child: Text("Upcoming Payment")),
+      child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(18, 10, 18, 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: FinColours.transactionBackground,
+                    ),
+                    height: 80,
+                    width: 300.w,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Maintenance April",
+                                style: TextStyle(
+                                    color: FinColours.grey, fontSize: 18),
+                              ),
+                              Text(
+                                "₹ 14,000",
+                                style: TextStyle(
+                                    color: FinColours.secondaryTextColor,
+                                    fontSize: 24),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Due",
+                                style: TextStyle(
+                                    color: FinColours.red, fontSize: 18),
+                              ),
+                              Text(
+                                "14 Apr",
+                                style: TextStyle(
+                                    color: FinColours.red, fontSize: 18),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
---
+++ lib/utils/colours.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/painting.dart';
+
+class FinColours {
+  static Color TextColor = Color(0xffe2e2e2);
+  static Color mainColor = Color(0xff483c94);
+  static Color grey = Color(0xff63636a);
+  static Color settingsBackground = Color(0xff483c94);
+  static Color secondaryTextColor = Color(0xff000000);
+  static Color secondaryColor = Color(0xffFFFFFF);
+  static Color transactionBackground = Color(0xffD4F4FF);
+  static Color transactionBackground2 = Color(0xffffcad0);
+  static Color transactionBackground3 = Color(0xffd0fdd5);
+  static Color transactionBackground4 = Color(0xfffbffd4);
+  static Color green = Color(0xff11ff00);
+
+  static Color red = Color(0xfff60256);
+}
